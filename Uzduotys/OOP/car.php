080fcb428bffbbc9a8550dce89e555fe5e32d1c7
Norbertas Jankauskas <norb.jankauskas@gmail.com>
2020-10-30 00:43:32 +0200

    keiciant kompa

---
+++ Uzduotys/OOP/car.php
@@ -0,0 +1,11 @@
+<?php
+
+    Class Car {
+        public string $spalva;
+
+        public string $greitis;
+
+        public function vaziuoti(): void {echo "automobilis vaziuoja"}
+
+        public function gautiSpalva(): string {}
+    }
---
+++ index.nav.php
@@ -0,0 +1,67 @@
+<html lang="lt">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Mano puslapis</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
+    <link rel="stylesheet" href="CSS/layout.nav.css">
+    <link rel="stylesheet" href="CSS/dizainas.nav.css">
+    <link rel="stylesheet" href="CSS/elementai.nav.css">
+</head>
+<body class="animate">
+
+<header>
+    <img src="https://picsum.photos/100/100" alt="logo" title="logo">
+    <nav>
+        <a href="#about" class="active">Apie</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#contacts">Kontaktai</a>
+    </nav>
+</header>
+<aside>
+    <section class="amalker">
+        <h4>Reklama:</h4>
+        <img src="https://picsum.photos/240/300" alt="Reklama">
+    </section>
+</aside>
+
+
+<main>
+    <section id="about">
+        <h1>Apie mane</h1>
+        <section>
+            <section class="portfolio_item" id="elementas">
+                <h4>Mano naujausias darbas:</h4>
+            </section>
+            <img src="https://picsum.photos/240/240" alt="Darbas" title="Darbas">
+            <img src="https://picsum.photos/240/240" alt="Norbertas Jankauskas" title="Norbertas Jankauskas">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem eos laboriosam ratione saepe! Autem delectus
+            distinctio, dolor dolorem ex ipsa labore quae quia tenetur velit! Ad adipisci cum eos ipsum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem eos laboriosam ratione saepe! Autem delectus
+            distinctio, dolor dolorem ex ipsa labore quae quia tenetur velit! Ad adipisci cum eos ipsum.
+        </section>
+    </section>
+    <section id="portfolio">
+        <h1>Portfolio</h1>
+    </section>
+    <section id="contacts">
+        <h1 id="forma">Kontaktai</h1>
+    </section>
+</main>
+<footer>
+    <span>
+        Visos teisės saugomos 2020&copy;
+    </span>
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script src="JS/teswt.js"></script>
+<script src="JS/navigacija.js"></script>
+<script src="JS/test_jquery.js"></script>
+<script src="JS/formSubmit.js"></script>
+
+</body>
+</html>
